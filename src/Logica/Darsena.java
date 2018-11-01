89b2473135f280052fb09203229b1f47400ec6c7
JuanLevis <juan.levis9425@gmail.com>
2018-11-01 17:58:46 -0300

    Modificaciones de tiempo en balanza y darsena

--- src/Logica/Darsena.java
+++ src/Logica/Darsena.java
@@ -6,6 +6,7 @@ public class Darsena {
     private int id;
     private long tiempoAtencion;
     private long proxFinAtencion;
+    private double randomAtencion;
     private Camion camion;
     private EstadoDarsena estadoDarsena;
 
@@ -19,6 +20,14 @@ public class Darsena {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public double getRandomAtencion() {
+        return randomAtencion;
+    }
+
+    public void setRandomAtencion(double randomAtencion) {
+        this.randomAtencion = randomAtencion;
     }
 
     public long getTiempoAtencion() {
@@ -68,7 +77,8 @@ public class Darsena {
 
     //Calcula el tiempo de atencion
     public void calcularTiempoAtencion() {
-        double demora = 15 + Math.random() * 5;
+        this.setRandomAtencion(Math.random());
+        double demora = 15 + this.getRandomAtencion() * 5;
         this.tiempoAtencion = (long) (demora / 60);
     }
 
