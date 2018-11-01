e48a01dfc20c289b7c5a584775f3671155183913
lucianoterziotti <lucianoterziotti@outlook.com>
2018-11-01 19:21:53 -0300

    Creacion de clase FinAtencion con su metodo ejecutar

---
+++ src/Logica/FinAtencionRecalibrado.java
@@ -0,0 +1,26 @@
+package Logica;
+
+public class FinAtencionRecalibrado extends Evento
+{
+    private Balanza balanza;
+
+
+    public  void ejecutar()
+    {
+        if(balanza.getCola().isEmpty())
+        {
+            balanza.setEstadoBalanza(EstadoBalanza.Libre);
+        }
+        else
+        {
+            balanza.setEstadoBalanza(EstadoBalanza.Ocupado);
+        }
+
+
+    }
+    public  String getNombre()
+    {
+        String nombre= "Fin At Recalibrado";
+        return nombre;
+    }
+}
