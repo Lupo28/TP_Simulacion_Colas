cb29407862302d574238590dc19d99c032412a52
lucianoterziotti <lucianoterziotti@outlook.com>
2018-11-05 20:55:54 -0300

    En clase Reloj, agregue horas = horas % 24, para que no se acumulen las horas y cambie de dia

--- src/Logica/Reloj.java
+++ src/Logica/Reloj.java
@@ -34,9 +34,9 @@ public class Reloj {
         double segundos = (long) (tiempoActual - (horas*3600 + minutos*60));
         String ceroH = "", ceroM = "", ceroS = "";
         if( horas < 10 ) ceroH = "0";
-        if(horas >=24)   horas = 0;
         if( minutos < 10 ) ceroM = "0";
         if( segundos < 10 ) ceroS = "0";
+        horas = horas % 24;
 
         return ceroH + String.valueOf(horas) + ":"  + ceroM + String.valueOf(minutos) + ":" + ceroS + String.valueOf(segundos);
     }
