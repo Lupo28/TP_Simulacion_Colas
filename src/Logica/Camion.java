800706d2df47b86b065eae253881b8f5bd9557ce
JuanLevis <juan.levis9425@gmail.com>
2018-11-12 19:26:26 -0300

    Añadido calculo de volumen de carga en Camion (TP6)

--- src/Logica/Camion.java
+++ src/Logica/Camion.java
@@ -4,18 +4,19 @@ public class Camion
 {
     private int numero;
     private EstadoCamion estado;
-
-
+    private double litrosCombustible;
 
     public Camion(int numero, EstadoCamion estado)
     {
         this.numero = numero;
         this.estado=estado;
+        generarLitrosCombustible();
     }
 
     public Camion(EstadoCamion estado){
         this.numero = 0;
         this.estado = estado;
+        generarLitrosCombustible();
     }
     public int getNumero() {
         return numero;
@@ -33,6 +34,17 @@ public class Camion
         this.estado = estado;
     }
 
+    public double getLitrosCombustible() {
+        return litrosCombustible;
+    }
+
+    public void setLitrosCombustible(double litrosCombustible) {
+        this.litrosCombustible = litrosCombustible;
+    }
+
+    public void generarLitrosCombustible(){
+        setLitrosCombustible(15000 + Math.random() * 5000);
+    }
 
     public String getNumeroString() {
 
